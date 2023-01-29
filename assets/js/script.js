e11d36e86ccf6b5d98252b054b9d9c9b343b6736
rafaelsoares07 <rafaelsoares1936@gmail.com>
2023-01-29 00:19:23 -0300

    feat: :sparkles: Adição de função javascript responsável pela interação com o slider da página

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+
+
+window.onload = function(){
+    // const controlRigth = document.querySelector(".arrow-right").click();
+
+    const controlRigth = document.querySelector(".arrow-right").innerHTML("<p>ola</p>")
+    
+    console.log(controlRigth)
+}
+
+controls.forEach((control) => {
+  control.addEventListener("click", (e) => {
+    let isLeft = e.target.classList.contains("arrow-left");
+
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    console.log(maxItems-1 + " :MAX")
+    console.log(currentItem + " :CUR")
+
+
+    if (currentItem >= maxItems-1) {
+      currentItem = 1;
+    }
+
+    if (currentItem <= 0) {
+      currentItem = maxItems - 2;
+    }
+
+
+    items.forEach((item) => item.classList.remove("current-item"));
+
+    items[currentItem].scrollIntoView({
+      behavior: "smooth",
+      inline: "center",
+      block:"nearest"
+    });
+
+    items[currentItem].classList.add("current-item");
+  });
+});
+
+
